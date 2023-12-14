212807feecb86c4c1318af675909fc147b7f70fc
zezo357 <zezo357@users.noreply.github.com>
2023-12-14 22:40:48 +0000

    deploy: 4c7514edb1cff3fb6290111f332578161017efd4

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "manifest.json": "1cdeadd17ac30faf1dc4e583f8f6ec19",
-"index.html": "a1f00de2ac98f636aa8867a6edf1857c",
-"/": "a1f00de2ac98f636aa8867a6edf1857c",
+"index.html": "926743ea41f8ed266e02b0d29eac8dfe",
+"/": "926743ea41f8ed266e02b0d29eac8dfe",
 "assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c",
 "assets/fonts/MaterialIcons-Regular.otf": "9169948c3cb73616cb04e1a2cbeeba10",
 "assets/AssetManifest.bin.json": "69a99f98c8b1fb8111c5fb961769fcd8",
