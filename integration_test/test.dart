0f1afc0c1926061d3d4ae2c349447d63fe03a930
Abdelaziz Mahdy <zezohassam@gmail.com>
2023-12-15 16:11:49 +0200

    fix screenshot

--- integration_test/test.dart
+++ integration_test/test.dart
@@ -1,9 +1,11 @@
 import 'dart:io';
 
 import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
+import 'package:integration_test/src/channel.dart';
 
 import 'package:bouncy_ball_physics/main.dart';
 
@@ -36,14 +38,55 @@ void main() {
   });
 }
 
-takeScreenshot(Widget app, String name, WidgetTester tester,
-    IntegrationTestWidgetsFlutterBinding binding) async {
-  if (kIsWeb) {
-    await binding.takeScreenshot(name);
-    return;
-  } else if (Platform.isAndroid) {
-    await binding.convertFlutterSurfaceToImage();
-    await tester.pumpFrames(app, const Duration(seconds: 1));
+Future<void> takeScreenshotForAndroid(
+    IntegrationTestWidgetsFlutterBinding binding, String name) async {
+  await integrationTestChannel.invokeMethod<void>(
+    'convertFlutterSurfaceToImage',
+    null,
+  );
+  binding.reportData ??= <String, dynamic>{};
+  binding.reportData!['screenshots'] ??= <dynamic>[];
+  integrationTestChannel.setMethodCallHandler((MethodCall call) async {
+    switch (call.method) {
+      case 'scheduleFrame':
+        PlatformDispatcher.instance.scheduleFrame();
+        break;
+    }
+    return null;
+  });
+  final List<int>? rawBytes =
+      await integrationTestChannel.invokeMethod<List<int>>(
+    'captureScreenshot',
+    <String, dynamic>{'name': name},
+  );
+  if (rawBytes == null) {
+    throw StateError(
+        'Expected a list of bytes, but instead captureScreenshot returned null');
   }
-  await binding.takeScreenshot(name);
+  final Map<String, dynamic> data = {
+    'screenshotName': name,
+    'bytes': rawBytes,
+  };
+  assert(data.containsKey('bytes'));
+  (binding.reportData!['screenshots'] as List<dynamic>).add(data);
+
+  await integrationTestChannel.invokeMethod<void>(
+    'revertFlutterImage',
+    null,
+  );
+}
+
+Future<void> takeScreenshot(
+  Widget app,
+  String name,
+  WidgetTester tester,
+  IntegrationTestWidgetsFlutterBinding binding,
+) async {
+  await tester.pumpFrames(app, const Duration(seconds: 1));
+  if (Platform.isAndroid) {
+    await takeScreenshotForAndroid(binding, name);
+  } else {
+    await binding.takeScreenshot(name);
+  }
+  print("Took screenshot: $name");
 }
